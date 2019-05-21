1cebf9be74ac15f5b2e5d417ed679a5a24c36d90
GaoX <GaoX@Gaox>
2019-05-21 17:52:41 +0800

    第一次

---
+++ src/cn/kgc/tangcco/anmeijiao/Test/Test.java
@@ -0,0 +1,10 @@
+package cn.kgc.tangcco.anmeijiao.Test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hhhh");
+		System.out.println("hhhh");
+		System.out.println("hhhh");
+	}
+
+}
